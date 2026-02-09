ff63472973f683b62cc4e5d19ea74f1836bb0480
Romans Mihalonoks <147718413+rrromanm@users.noreply.github.com>
2026-02-09 21:45:33 +0100

    cors fix 2

--- backend/src/main/java/com/mbpluslevante/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/mbpluslevante/backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.mbpluslevante.backend.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,7 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> {})
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm ->
                         sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
