710972b6be1ba3bb13071cf926fa08928c6c6458
Romans Mihalonoks <147718413+rrromanm@users.noreply.github.com>
2026-02-15 11:32:34 +0100

    remove languages for v1

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,18 @@
-import LanguageSelector from "./LanguageSelector";
 import Image from "next/image";
+import { Switch } from "@/components/ui/switch";
+import { Moon } from "lucide-react";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 h-25 bg-[#1D1D1D] text-white shadow-md">
       <div className="mx-auto flex h-full items-center justify-between px-16">
         <div className="flex items-center gap-2">
-          <Image src="/mb-plus-logo.svg" alt="MB Plus Logo" width={160} height={60} />
+          <Image
+            src="/mb-plus-logo.svg"
+            alt="MB Plus Logo"
+            width={160}
+            height={60}
+          />
         </div>
 
         <nav className="hidden md:flex items-center gap-6 text-md font-medium text-white">
@@ -22,7 +28,8 @@ export default function Header() {
         </nav>
 
         <div className="flex items-center gap-4">
-          <LanguageSelector />
+          <Switch id="theme-toggle"/>
+          <Moon size={16} />
         </div>
       </div>
     </header>
